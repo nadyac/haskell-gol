bf267ea2d3748f5addeaecef6106298c0a56f7f2
nadyac <nadyacpena@gmail.com>
2015-10-19 23:39:24 -0400

    updated haskell project

---
+++ src/AddNegatives.java
@@ -0,0 +1,14 @@
+public class AddNegatives{
+	
+	public static void main(String[] args){
+		
+		int a = 2;
+		int b = -1000;
+		int c = a + b;
+		System.out.println(c);
+		
+		c = a - b;
+		System.out.println(c);
+	}
+
+}
--- src/haskell-gol.hs
+++ src/haskell-gol.hs
@@ -1,16 +1,79 @@
 module Main where
 
 import Data.List
-import Data.Map hiding (map)
+import Data.Map hiding (map, foldl)
 import Data.Char
 import Data.Maybe
 import Text.Printf
 import System.IO  
 import Control.Monad
 
+-- http://hayoo.fh-wedel.de/?query=replicate
+
+-- create a type Matrix which is just a list of characters
+type Matrix = [[Char]]
+
+-- Create a list of 10 characters, which are 10 dots
+base_matrix :: Matrix
+base_matrix = replicate 10 . replicate 10 $ '.'
+
+-- display the matrix
+showMatrix :: Matrix -> String
+showMatrix m = unlines . map showRow $ m
+    where
+        showRow r = r
+
+printMatrix :: Matrix -> IO ()
+printMatrix x = putStrLn (showMatrix x)
+
+{-
+[1, 2, 3, 4, 5]
+[1,2] [3,4,5]
+[1,2] ++ [6] ++ [4,5]
+-}
+
+replaceNth :: [a] -> a -> Int -> [a]
+replaceNth xs e i = left ++ [e] ++ (tail right)
+    where
+        (left, right) = splitAt i xs
+
+replace :: (Int, Int) -> Matrix -> Matrix
+replace (x,y) m = replaceNth m newRow y
+    where
+        origRow = m !! y
+        newRow = replaceNth origRow '0' x
+
+--[....
+-- ....
+-- ....
+--]
+ 
+--replace2 :: (Int, Int) -> [Int] -> [Int]
+--replace2 (x,y) m = replaceNth (y * 10 + x) '0' m
+
+read_file :: IO ()
+read_file = do
+    fileContents <- readFile "grades.txt"
+    let file_lines = lines fileContents
+    let tuples = (read . head $ file_lines :: [(Int, Int)])
+    
+    {-
+    let replacedMatrix = flip execState base_matrix $ do
+        mapM_ (\t -> modify $ replace t) tuples
+    -}
+    
+    -- foldl :: (b -> a -> b) -> b -> [a] -> b
+    -- sum xs = fold (+) 0 xs
+    -- foldl :: (Matrix -> (Int,Int) -> Matrix) -> Matrix -> [(Int, Int)] -> Matrix
+    
+    let replacedMatrix = foldl (flip replace) base_matrix tuples
+    
+    printMatrix replacedMatrix
+    
+    
+    print "Done."
+
 -- Print out the contents of the file
 main :: IO()
-main = do  
-	fileHandle <- readFile "grades.txt"
-	print fileHandle
-	print "Done."
+main = do
+        read_file
